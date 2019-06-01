5afdf172f03d0d5b2ac651b5d922ecd8084c52c9
Junjie Chen <junjie.chen18@gmail.com>
2019-06-01 15:33:39 -0400

    feat(src): update state with fetched images

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,15 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  state = { images: [] };
+
   onSearchSubmit = async term => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: { Authorization: 'Client-ID YOUR_ACCESS_KEY' }
     });
+
+    this.setState({ images: response.data.results });
   };
 
   render() {
