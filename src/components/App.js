a564fa25e4f9dcdd3fee3421a91236359fdc1c96
Junjie Chen <junjie.chen18@gmail.com>
2019-05-31 22:37:04 -0400

    feat(src): send and receive requests with async function

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit = term => {
-    axios.get('https://api.unsplash.com/search/photos', {
+  onSearchSubmit = async term => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: { Authorization: 'Client-ID YOUR_ACCESS_KEY' }
     });
