6e8cb5f32b9c1160235545a5459c81727858088f
Junjie Chen <junjie.chen18@gmail.com>
2019-06-02 13:42:52 -0400

    feat(src): add key prop to a list of images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,6 +4,7 @@ const ImageList = props => {
   const images = props.images.map(image => {
     return (
       <img
+        key={image.id}
         src={image.urls.regular}
         alt={image.description}
       />
