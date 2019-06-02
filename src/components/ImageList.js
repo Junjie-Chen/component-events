f058e0fd6cae5799fa937542f7293ec895159b54
Junjie Chen <junjie.chen18@gmail.com>
2019-06-02 13:03:35 -0400

    feat(src): show a list of images in the browser

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const ImageList = props => {
-  return <div>Image List</div>;
+  const images = props.images.map(image => {
+    return (
+      <img
+        src={image.urls.regular}
+        alt={image.description}
+      />
+    );
+  });
+
+  return <div>{images}</div>;
 };
 
 export default ImageList;
