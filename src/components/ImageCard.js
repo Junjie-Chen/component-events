8f6954859beaaf73272e8a21015353ae40882bae
Junjie Chen <junjie.chen18@gmail.com>
2019-06-08 15:59:56 -0400

    feat(src): create reference to images inside of constructor function

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
   render() {
     const { urls, description } = this.props.image;
 
