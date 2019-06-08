c54ee39e35428342d50173fe6dfeba1ae4402c51
Junjie Chen <junjie.chen18@gmail.com>
2019-06-08 13:29:33 -0400

    feat(src): pass image prop to ImageCard component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,12 +3,11 @@ import React from 'react';
 import ImageCard from './ImageCard';
 
 const ImageList = props => {
-  const images = props.images.map(({ id, urls, description }) => {
+  const images = props.images.map(image => {
     return (
       <ImageCard
-        key={id}
-        src={urls.regular}
-        alt={description}
+        key={image.id}
+        image={image}
       />
     );
   });
